4fcf1e80a99f5e18d78fb21dd08d82059d9ca083
Rafael-Ramildes-Ferreira <rafael.r.ferreira@grad.ufsc.br>
2025-11-12 20:01:32 -0300

    Initial commit: Basic thread class implementation

---
+++ app_main.cpp
@@ -0,0 +1,12 @@
+#include "threads/threads.hpp"
+
+void app_main(){
+	return;
+}
+
+#ifdef __linux__
+int main(){
+	app_main();
+	return 0;
+}
+#endif // __linux__
---
+++ threads/threads.hpp
@@ -0,0 +1,36 @@
+#ifndef __THREADS_HPP
+#define __THREADS_HPP
+
+/**
+ * @class BaseThread
+ * @brief Defines the interface of a thread class
+ * 		Can be enforeced in compiletime after C++20
+ */
+class BaseThread
+{
+public:
+	virtual void run() = 0;
+	virtual void dispatch() = 0;
+	virtual void setup() = 0;
+	virtual void loop() = 0;
+};
+
+/**
+ * @interface Defines the interface of the thread
+ * @brief Inforces a thread class follows the interface
+ * 		(only after C++20)
+ */
+#if __cplusplus > 202002L
+template<class Base>
+concept ThreadInterface = 
+	std::is_base_of_v<SuperBase,Base> &&
+    !std::is_same_v<SuperBase,Base>;
+#else
+#define ThreadInterface class
+#endif
+
+class FreeRTOSThread : public BaseThread {};
+
+class PthreadClass : public BaseThread {};
+
+#endif // __THREADS_HPP
